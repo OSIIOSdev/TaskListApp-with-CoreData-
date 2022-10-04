6ee58e386a952200f5f63ec21e3615983efafca8
Ilya Osipov <osiios.dev@gmail.com>
2022-10-04 15:34:19 +0300

    1. TaskListViewController has been added 2. TaskViewController has been added 3. TabBar has been configured

---
+++ CoreDataApp/TaskViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TaskViewController.swift
+//  CoreDataApp
+//
+//  Created by Илья on 04.10.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+    
+}

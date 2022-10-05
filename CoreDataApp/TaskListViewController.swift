56134478308ca57820d965b26cb6d7185f82f97d
Ilya Osipov <osiios.dev@gmail.com>
2022-10-05 12:17:28 +0300

    Final project without	delete and editing function

--- CoreDataApp/TaskListViewController.swift
+++ CoreDataApp/TaskListViewController.swift
@@ -61,10 +61,7 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        let task = TaskViewController()
-        task.modalPresentationStyle = .fullScreen
-        
-        present(task, animated: true)
+        showAlert(with: "New task", and: "What do you want to do?")
     }
     
     private func fetchData() {
@@ -79,8 +76,6 @@ class TaskListViewController: UITableViewController {
 }
 
 extension TaskListViewController {
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         taskList.count
     }
@@ -94,6 +89,44 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         
         return cell
+    }
+}
+
+extension TaskListViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty
+            else { return }
+            
+            self.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New task"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        let task = Task(context: context)
+        task.name = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
     }
 }
 
